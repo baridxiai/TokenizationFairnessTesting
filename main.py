2dd63dbcd681405a91bf20c742796454c21b95aa
vivalavida <barid.x.ai@gmail.com>
2025-05-01 02:43:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ class TokenPruner(nn.Module):
 
         # Top-k token indices
         _, topk_ids = torch.topk(
-            importance_scores, compressed_length,  sorted=False
+            importance_scores, compressed_length, sorted=False
         ) # [seq_len]
         # Sort to preserve order
         # topk_ids = torch.sort(topk_ids, dim=1)[0] # [1, seq_len]
